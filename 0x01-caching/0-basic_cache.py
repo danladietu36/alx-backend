96cafa8eaaa0e183d51b91570afb72fdebcced1d
Danladi Etu <danladietugithub@example.com>
2023-08-24 12:46:31 +0000

    create caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from base_caching import BaseCaching
+
+
+class BsicCache(BaseCaching):
+    """A class that represent adding and retrieval of
+       objects from a cache via their keys
+    """
+
+    def put(self, key, item):
+        """Method to add item to the
+        """
+        if key is None or item not in self.cache_data:
+            return
+        self.cache_data = item
+
+    def get(self, key):
+        """Method to return an item.
+        """
+        return self.cache_data(key, None)
