3675c947ba92ca5e156c8f6a5d19d19c83be56dd
Danladi Etu <danladietugithub@example.com>
2023-08-25 20:35:57 +0000

    added

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,21 +1,21 @@
-i#!/usr/bin/env python3
-""" pagination module"""
+#!/usr/bin/env python3
+"""
+Deletion-resilient Module for pagination
+"""
 
-from typing import Tuple, List, Dict, Any
 import csv
 import math
-
-
-index_range = __import__("0-simple_helper_function").index_range
+from typing import List, Dict
 
 
 class Server:
-    """Server class that paginate a database of popular babies names.
+    """A class to paginate atabase of popular babies names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
+        self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
         """Cached dataset function
@@ -28,33 +28,35 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ function that returnelements with a pagination orderss """
-        assert type(page) == int and page > 0
-        assert type(page_size) == int and page_size > 0
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Function for Dataset indexed by sorting position, starting at 0 (zero)
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
 
-        start, end = index_range(page, page_size)
-        res_list = []
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Function that return all data"""
+        assert type(index) == int and type(page_size) == int
+        assert 0 <= index < len(self.indexed_dataset())
 
-        if start >= len(self.dataset()):
-            return res_list
-        res_list = self.dataset()
-        return res_list[start:end]
+        data = []
+        next_index = index + page_size
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
-        """function that returns a object """
-        assert type(page) == int and page > 0
-        assert type(page_size) == int and page_size > 0
-
-        total_pages = int(len(self.dataset()) / page_size)
-        next_page = page + 1 if (page + 1) < total_pages else None
-        prev_page = page - 1 if page > 1 else None
+        for i in range(index, next_index):
+            if self.indexed_dataset().get(i):
+                data.append(self.indexed_dataset()[i])
+            else:
+                i += 1
+                next_index += 1
 
         return {
-            "page_size": len(self.get_page(page, page_size)),
-            "page": page,
-            "data": self.get_page(page, page_size),
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages
+            'data': data,
+            'index': index,
+            'next_index': next_index,
+            'page_size': page_size
         }
